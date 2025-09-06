e57416c369dab94e1dfbc7088123ef34d38911af
jishnu2021 <jishnughosh98@gmail.com>
2025-09-06 23:49:21 +0530

    add frontend origin

--- serverside/index.js
+++ serverside/index.js
@@ -49,8 +49,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
-    credentials: true, 
+    origin: [
+      "http://localhost:5173",            // local dev
+      "https://moneymanager-wgca.onrender.com"  // deployed frontend domain
+    ],
+    credentials: true,
   })
 );
 
